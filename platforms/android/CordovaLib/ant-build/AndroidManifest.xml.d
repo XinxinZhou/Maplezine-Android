04019245afdfe0004c7b1375c1a88e22476b1662
xinxin zhou <a39620@163.com>
2015-03-13 13:59:07 -0400

    Revert "Milestone1"
    This reverts commit 38a9040d753c860b0326473c52cf94d74536fa95.

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-F:\workspace\GitHub\OttawaZineApps-Android-effective - 副本-3.2\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-F:\workspace\GitHub\OttawaZineApps-Android-effective - 副本-3.2\platforms\android\CordovaLib\AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-F:\workspace\GitHub\OttawaZineApps-Android-effective - 副本-3.2\platforms\android\ant-build\AndroidManifest.xml : \
-F:\workspace\GitHub\OttawaZineApps-Android-effective - 副本-3.2\platforms\android\AndroidManifest.xml \
